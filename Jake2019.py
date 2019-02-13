4b465a5ffe3d345524e017af9ce4e05cd998f3f2
Josh Hartmann <jchart@uvic.ca>
2019-02-12 18:14:32 -0800

    Almost as good as Mitchell Nursey, checkpoint before trying to floodfill my way out of traps

--- Jake2019.py
+++ Jake2019.py
@@ -2,9 +2,16 @@ import os
 import random
 import time
 
-STARVING = 45
-HUNGRY = 55
-MEH_COULD_EAT = 60
+DEBUG = True
+
+# The higher the quicker we start searching for food
+HUNGRY = 60
+# Starting search radius
+FOOD_MIN = 1
+# When we start searching our max radius
+STARVING = 15
+# Max searching radius
+FOOD_MAX = 10
 
 def move(data):
     # Get all the data
@@ -21,85 +28,113 @@ def move(data):
     snakes = [s['body']['data'] for s in snakes]
     snakes2 = []
     heads = []
+    tails = []
     for s1 in snakes:
         heads.append((s1[0]['x'], s1[0]['y']))
+        tails.append((s1[-1]['x'], s1[-1]['y']))
         for s2 in s1:
             snakes2.append((s2['x'], s2['y']))
     snakes = snakes2
     food = data['food']['data']
     food = [(f['x'], f['y']) for f in food]
     numFood = len(food)
-    try:
-        pm = get_previous_move(head, body[1])
-    except:
-
-        moves = get_restrictions(head, mySize, walls, snakes, heads, size)
-        if moves == []:
-            return {"move": "up"}
-        else:
-            return {"move": random.choice(moves)}
-
 
     try:
         move = None
 
         # Moving restrictions
-        moves = get_restrictions(head, mySize, walls, snakes, heads, size)
+        if mySize > 2:
+            moves = get_restrictions(head, [body[-1]], mySize, walls, snakes, heads, size)
+        else:
+            moves = get_restrictions(head, [], mySize, walls, snakes, heads, size)
+        debug_print("Restrictions:  ", moves)
 
         # Don't choose nothing that'll kill you next time
-        if ( len(moves) > 1 ):
+        if len(moves) > 1:
 
             movesCpy = list(moves)
             for m in movesCpy:
                 nextHead = get_future_head(head, m)
-                nres = get_restrictions(nextHead, mySize, walls, snakes, heads, size, op=False)
-                if nres == [] and len(moves) > 1:
+                if mySize > 2:
+                    nres = get_restrictions(nextHead, tails + [body[-2]], mySize, walls, snakes, heads, size, op=False)
+                else:
+                    nres = get_restrictions(nextHead, list(tails).remove(body[-1]), mySize, walls, snakes, heads, size, op=False)
+                if (nres == []) and (len(moves) > 1):
                     moves.remove(m)
+                    debug_print("Restrictions2: ", moves)
+
+
 
         # Take food as first preference if health is low
-        if(have_choice(move, moves) and health < STARVING):
-            moves = get_food(moves, head, food, 3)
+        if have_choice(move, moves) and (health < HUNGRY):
+            maxFood = round( (1 - ((health-STARVING) / (HUNGRY-STARVING))) * (FOOD_MAX-FOOD_MIN) )
 
-        if(have_choice(move, moves) and health < HUNGRY):
-            moves = get_food(moves, head, food, 2)
+            for i in reversed(range(1, maxFood)):
+                if have_choice(move, moves):
+                    moves = get_food(moves, head, food, i)
+                    debug_print("Gimme Brunch {}:".format(i), moves)
 
         # Flee from a wall as preference
-        if(have_choice(move, moves)):
+        if have_choice(move, moves):
             moves = flee_wall(moves, walls, head)
+            debug_print("Flee Wall:     ", moves)
 
         # Flee others (including yourself) as preference
-        if(have_choice(move, moves)):
+        if have_choice(move, moves):
             moves = flee_others(moves, [body[0], body[-1]], snakes, head, 1)
+            debug_print("Flee Others:   ", moves)
 
         # Take killing others as preference
-        if(have_choice(move, moves)):
+        if have_choice(move, moves):
             moves = kill_others(head, mySize, heads, size, moves)
-
-        # Take local food as preference if health could use a touchup
-        if(have_choice(move, moves) and health < MEH_COULD_EAT):
-            moves = get_food(moves, head, food, 1)
+            debug_print("Kill Others:   ", moves)
 
         # Go straight as preference
-        if(pm in moves or moves == []):
-            move = pm
+        if have_choice(move, moves):
+            move = go_straight(moves, head, body)
+            debug_print("Go Straight:   ", move)
+
+        # Move away from the heads of others
+        if have_choice(move, moves):
+            move = flee_heads(moves, heads, head)
+            debug_print("Flee Heads:    ", move)
 
         # Make a random choice for a move
-        else:
+        if have_choice(move, moves):
             move = random.choice(moves)
+        else:
+            if len(moves) == 0:
+                move = eat_tail(head, tails)
+                debug_print("Eat Tail:      ", move)
+                if move == None:
+                    move = 'up'
+            else:
+                move = moves[0]
 
     except Exception as e:
         raise(e)
+        print(e)
         if moves == []:
             move = "up"
         else:
             move = random.choice(moves)
+
+    debug_print("MOVE: ", move)
 
     return {
         'move': move,
         'taunt': 'Battle Jake!'
     }
 
+
+def debug_print(*args, **kwargs):
+    if DEBUG:
+        print(*args, **kwargs)
+
+
 def have_choice(move, moves):
+    if move != None:
+        return False
     if len(moves) <= 1:
         return False
     return True
@@ -107,13 +142,13 @@ def have_choice(move, moves):
 
 def get_future_head(head, move):
 
-    if(move == 'left'):
+    if move == 'left':
         return (head[0] - 1, head[1])
 
-    elif(move == 'right'):
+    elif move == 'right':
         return (head[0] + 1, head[1])
 
-    elif(move == 'up'):
+    elif move == 'up':
         return (head[0], head[1] - 1)
 
     else:
@@ -122,18 +157,65 @@ def get_future_head(head, move):
 
 def get_previous_move(head, second):
 
-    if(head[0] == second[0]):
-        if(head[1] > second[1]):
+    if head[0] == second[0]:
+        if head[1] > second[1]:
             return 'down'
 
         else:
             return 'up'
     else:
-        if(head[0] > second[0]):
+        if head[0] > second[0]:
             return 'right'
 
         else:
             return 'left'
+
+
+def eat_tail(head, tails):
+    for tail in tails:
+        xdist = head[0] - tail[0]
+        ydist = head[1] - tail[1]
+        if abs(xdist) == 1 and ydist == 0:
+            if xdist > 0:
+                return 'left'
+            else:
+                return 'right'
+
+        if abs(ydist) == 1 and xdist == 0:
+            if ydist > 0:
+                return 'up'
+            else:
+                return 'down'
+
+
+def go_straight(moves, head, body):
+    if len(body) > 1:
+        pm = get_previous_move(head, body[1])
+        if pm in moves:
+            return pm
+
+
+def flee_heads(moves, heads, head):
+    minManhattan = 999
+    for h in heads:
+        xdist = h[0]-head[0]
+        ydist = h[1]-head[1]
+
+        manhattan = abs(xdist) + abs(ydist)
+        if manhattan < minManhattan:
+            minManhattan = manhattan
+            if (abs(xdist) < abs(ydist)):
+                if ('left' in moves) and (xdist > 0):
+                    return 'left'
+
+                if ('right' in moves) and (xdist < 0):
+                    return 'right'
+            else:
+                if ('down' in moves) and (ydist < 0):
+                    return 'down'
+
+                if ('up' in moves) and (ydist > 0):
+                    return 'up'
 
 
 def flee_others(moves, delMoves, snakes, head, dist):
@@ -160,40 +242,40 @@ def flee_others(moves, delMoves, snakes, head, dist):
 def flee_wall(moves, walls, head):
 
     # Flee the wall if I'm against it
-    if(head[0] >= walls[0]-1):
+    if head[0] >= walls[0]-1:
         if 'left' in moves:
             return ['left']
 
-    elif(head[0] <= 0):
+    elif head[0] <= 0:
         if 'right' in moves:
             return ['right']
 
-    if(head[1] <= 0):
+    if head[1] <= 0:
         if 'down' in moves:
             return ['down']
 
-    elif(head[1] >= walls[1]-1):
+    elif head[1] >= walls[1]-1:
         if 'up' in moves:
             return ['up']
 
     validMoves = list(moves)
 
     # Keep 1 space buffer between you and the wall
-    if(head[0] >= walls[0]-2):
+    if head[0] >= walls[0]-2:
         if 'right' in moves:
             validMoves.remove('right')
 
-    elif(head[0] <= 1):
+    elif head[0] <= 1:
         if 'left' in moves:
             validMoves.remove('left')
 
     if len(moves) > 1:
 
-        if(head[1] <= 1):
+        if head[1] <= 1:
             if 'up' in moves:
                 validMoves.remove('up')
 
-        elif(head[1] >= walls[1]-2):
+        elif head[1] >= walls[1]-2:
             if 'down' in moves:
                 validMoves.remove('down')
 
@@ -210,42 +292,42 @@ def kill_others(head, mySize, heads, size, moves):
 
     for i, h in enumerate(heads):
 
-        if(size[i] < mySize):
+        if size[i] < mySize:
 
             xdist = h[0]-head[0]
             ydist = h[1]-head[1]
 
-            if(abs(xdist) == 1 and abs(ydist) == 1):
+            if (abs(xdist) == 1) and (abs(ydist) == 1):
 
-                if(xdist > 0 and 'right' in moves):
+                if xdist > 0 and 'right' in moves:
                     validMoves.append('right')
 
-                elif(xdist < 0 and 'left' in moves):
+                elif xdist < 0 and 'left' in moves:
                     validMoves.append('left')
 
-                if(ydist > 0 and 'down' in moves):
+                if ydist > 0 and 'down' in moves:
                     validMoves.append('down')
 
-                elif(ydist < 0 and 'up' in moves):
+                elif ydist < 0 and 'up' in moves:
                     validMoves.append('up')
 
-            elif((abs(xdist) == 2 and ydist == 0) ^ (abs(ydist) == 2 and xdist == 0)):
+            elif (abs(xdist) == 2 and ydist == 0) or (abs(ydist) == 2 and xdist == 0):
 
-                if(xdist == 2 and 'right' in moves):
+                if xdist == 2 and 'right' in moves:
                     validMoves.append('right')
 
-                elif(xdist == -2 and 'left' in moves):
+                elif xdist == -2 and 'left' in moves:
                     validMoves.append('left')
 
-                elif(ydist == 2 and 'down' in moves):
+                elif ydist == 2 and 'down' in moves:
                     validMoves.append('down')
 
-                elif('up' in moves):
+                elif 'up' in moves:
                     validMoves.append('up')
 
     if validMoves == []:
         return moves
-    return validMoves
+    return list(set(validMoves))
 
 
 def get_food(moves, head, food, dist):
@@ -256,104 +338,105 @@ def get_food(moves, head, food, dist):
         xdist = f[0]-head[0]
         ydist = f[1]-head[1]
 
-        if((abs(xdist) == dist and ydist == 0) ^ (abs(ydist) == dist and xdist == 0)):
+        if (abs(xdist) + abs(ydist)) <= dist:
 
-            if(xdist == dist and 'right' in moves):
+            if xdist > 0 and 'right' in moves:
                 validMoves.append('right')
 
-            elif(xdist == -dist and 'left' in moves):
+            elif xdist < 0 and 'left' in moves:
                 validMoves.append('left')
 
-            elif(ydist == dist and 'down' in moves):
+            elif ydist > 0 and 'down' in moves:
                 validMoves.append('down')
 
-            elif(ydist == -dist and 'up' in moves):
+            elif ydist < 0 and 'up' in moves:
                 validMoves.append('up')
 
     if validMoves == []:
         return moves
-    return validMoves
+    return list(set(validMoves))
 
 
-def get_restrictions(head, mySize, walls, snakes, heads, size, op=True):
+def get_restrictions(head, ignore, mySize, walls, snakes, heads, size, op=True):
 
     directions = {'up':1, 'down':1, 'left':1, 'right':1}
 
     # Don't hit a wall
-    if(head[0] == walls[0]-1):
+    if head[0] == walls[0]-1:
         directions['right'] = 0
 
-    elif(head[0] == 0):
+    elif head[0] == 0:
         directions['left'] = 0
 
-    if(head[1] == 0):
+    if head[1] == 0:
         directions['up'] = 0
 
-    elif(head[1] == walls[1]-1):
+    elif head[1] == walls[1]-1:
         directions['down'] = 0
 
-    # Don't hit other snakes
+    # Don't hit other snakes (Except for exceptions (tail etc))
     for s in snakes:
-        xdist = abs(s[0]-head[0])
-        ydist = abs(s[1]-head[1])
+        if s not in ignore:
+            xdist = abs(s[0]-head[0])
+            ydist = abs(s[1]-head[1])
 
-        if(xdist + ydist == 1):
+            if xdist + ydist == 1:
 
-            if(xdist == 1):
+                if xdist == 1:
 
-                if(s[0] > head[0]):
-                    directions['right'] = 0
+                    if s[0] > head[0]:
+                        directions['right'] = 0
 
-                else:
-                    directions['left'] = 0
-
-            else:
-
-                if(s[1] > head[1]):
-                    directions['down'] = 0
+                    else:
+                        directions['left'] = 0
 
                 else:
-                    directions['up'] = 0
+
+                    if s[1] > head[1]:
+                        directions['down'] = 0
+
+                    else:
+                        directions['up'] = 0
 
     directions2 = {key: value for key, value in directions.items()}
 
     # Be scared of the heads of others if they're scary
     for i, h in enumerate(heads):
 
-        if(not (size[i] < mySize)):
+        if not (size[i] < mySize):
             xdist = h[0]-head[0]
             ydist = h[1]-head[1]
 
-            if(abs(xdist) == 1 and abs(ydist) == 1):
+            if abs(xdist) == 1 and abs(ydist) == 1:
 
-                if(xdist > 0):
+                if xdist > 0:
                     directions['right'] = 0
 
-                elif(xdist < 0):
+                elif xdist < 0:
                     directions['left'] = 0
 
-                if(ydist > 0):
+                if ydist > 0:
                     directions['down'] = 0
 
-                elif(ydist < 0):
+                elif ydist < 0:
                     directions['up'] = 0
 
-            elif((abs(xdist) == 2 and ydist == 0) ^ (abs(ydist) == 2 and xdist == 0)):
+            elif (abs(xdist) == 2 and ydist == 0) ^ (abs(ydist) == 2 and xdist == 0):
 
-                if(xdist == 2):
+                if xdist == 2:
                     directions['right'] = 0
 
-                elif(xdist == -2):
+                elif xdist == -2:
                     directions['left'] = 0
 
-                elif(ydist == 2):
+                elif ydist == 2:
                     directions['down'] = 0
 
                 else:
                     directions['up'] = 0
 
     # If there's no other choice but to possibly collide with a head
-    if(1 not in directions.values() and op):
+    if 1 not in directions.values() and op:
         directions = directions2
 
     if not op:
