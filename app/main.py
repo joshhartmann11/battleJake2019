dc40fe7ef082414f0938cbd564e02c6e61f58829
Joshua Hartmann <jchart@uvic.ca>
2019-02-28 03:09:04 -0800

    auto-commit

--- app/main.py
+++ app/main.py
@@ -51,10 +51,11 @@ def move(data=None):
     head = body[0]
     walls = (data['board']['width'], data['board']['height'])
 
-    snakes = [ list(set([ ( b['x'], b['y'] ) for b in s['body'] ])) for s in data['board']['snakes'] ]
-    heads = [ s[0] for s in snakes ]
-    tails = [ s[-1] for s in snakes ]
-    size = [ len(s) for s in snakes ]
+    snakesTogether = [ list(set([ ( b['x'], b['y'] ) for b in s['body'] ])) for s in data['board']['snakes'] ]
+    heads = [ s[0] for s in snakesTogether ]
+    tails = [ s[-1] for s in snakesTogether ]
+    size = [ len(s) for s in snakesTogether ]
+    snakes = list(set( [ ( b['x'], b['y'] ) for b in s['body'] for s in data['board']['snakes'] ] ))
 
     food = [(f['x'], f['y']) for f in data['board']['food']]
     numFood = len(food)
